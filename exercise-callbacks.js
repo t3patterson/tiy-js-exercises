412839d7ac171b76ce849bafcfe24ea7cfbb4fe7
travis <t3patterson@gmail.com>
2016-01-30 14:34:28 -0600

    adding some callback exercises

--- exercise-callbacks.js
+++ exercise-callbacks.js
@@ -25,7 +25,8 @@ function runEvenOrOdd(cb1,cb2){
   }
 }
 
-// Exercise 3 (callbacks passing object to make different string) 
+
+// Exercise 3 ( examination of passing an array to a callback ) 
 // ------------------------------------------
 // Write a function that takes an array as the 1st argument and a callback_fn as the 2nd
 // ...the callback_fn should receive as parameters:
@@ -35,4 +36,90 @@ var someNames = ["Jimmy", "Sally", "Georgie"]
 
 function doSthToFirstElement(arr, fn){
   fn(arr[0],0,arr);
+}
+
+//Exercise 4 (iterating over an array of functions and executing them one by one)
+// ------------------------------------------
+// Create a function that accepts an array of functions. 
+//   The function you build will execute each function in the array.
+
+var myListOfTasks = [
+  function(){ console.log('hey mister'); },
+  function(){ alert('this is gonna hurt real bad');},
+  function(){ console.log('why did i sign up for this?');}
+]
+
+function runAllTehCallbacks(arrOfFun){
+  for (var i = 0 i < arrOfFun.length; i++){
+    arrOfFun[i]()
+  }
+}
+
+//Exercise 5 (iterating over an array of callbacks that expects & accepts a string as an argument)
+// ------------------------------------------
+// Create a function that accepts a string and an array of functions. 
+//   The function will execute each function within the array AND pass each function the string argument
+
+var anotherListOfTasks = [
+  function(str){ alert('so your name is <' + str +'>'); },
+  function(word){ console.log('wow cool okay so the word <'+word+'> has <'+ str.length + '> characters'); },
+  function(arg){ prompt('Are you starting to notice that we can do a lot with callback-functions + the argument: '+arg+'?', '');}
+  function(something){ console.log("and in this case, the string is passed but nothing is done with it.")}
+]
+
+function runCallbacksPassString(myString ,arrOfFun){
+  for (var i = 0 i < arrOfFun.length; i++){
+    arrOfFun[i](myString)
+  }
+}
+
+//Exercise 6 (validate object and execute cb with object as parameter)
+//----------------------------------------
+// Create a function that accepts 2 arguments: an object and a callback
+//    Create an if-statement evaluate whether the object's `.accepted` property evaluates to `true`, 
+//    and if, so execute the callback WITH the object passed as an argument
+
+function passMeAsCb(o) {
+  console.log( 'allllrigght! <'+ o.name + '> is part of the super-secret club!' )
+  console.log( 'Age in dog years: <'+ o.age/7 +'>' )
+  console.log(' ----------+----------')
+}
+
+function alsoPassMeAsCb(o) {
+   alert("looks like i've been executed but i'm not gonna do anything with the arg passed to me");  
+}
+
+var person1 = {
+  name: 'Carol',
+  age: 33,
+  haircut: 'mullet',
+  accepted: true
+}
+
+var person2 = {
+  name: "Gary"
+  children: ['Gracie', 'Willy'],
+  age: 47,
+  accepted: false
+}
+
+var person3 = {
+  name: 'Shamu',
+  age: 22,
+  haircut: 'not sure, but whale-like'
+  accepted: true
+}
+
+var person4 = {
+  name: 'Ice Age Caveman',
+  age: 10006,
+  accepted: false
+}
+
+//Write your function down here -----
+
+function runOnlyWhen(obj,cb){
+  if (obj.accepted === true){
+    cb(obj)
+  }
 }
